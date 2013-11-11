a1ff29ae2edbdb1260c42fcf0242cf9cd6b41373
James Davis <jamesmdavis5@gmail.com>
2013-11-11 15:43:53 -0500

    working on signup dropdown, this version is ok with the hover, committing now to make some changes

--- app/assets/javascripts/market.js
+++ app/assets/javascripts/market.js
@@ -1,3 +1,56 @@
 $(document).ready(function() {
     $('#market').accordion();
+
+$(".login-btn").hover(
+    function() {
+        clearTimeout($(this).data('hoverTimeoutId'));
+        $(".login-content").show();
+        $(this).addClass('hovered');
+    },
+    function() {
+        clearTimeout($(this).data('hoverTimeoutId'));
+        $(this).data('hoverTimeoutId', setTimeout(function () {
+            $(".login-content").hide();
+            $(this).removeClass('hovered');
+        } ,500));
+    });
+
+
+$('.login-content').hover(
+    function(){
+        clearTimeout($(".login-btn").data('hoverTimeoutId'));
+    },
+    function(){
+        $(".login-content").hide();
+        $(".login-btn").removeClass('hovered');
+    });
+
+// Can probably refactor this since it is the same for both login and signup
+
+$(".signup-btn").hover(
+    function() {
+        clearTimeout($(this).data('hoverTimeoutId'));
+        $(".signup-content").show();
+        $(this).addClass('hovered');
+    },
+    function() {
+        clearTimeout($(this).data('hoverTimeoutId'));
+        $(this).data('hoverTimeoutId', setTimeout(function () {
+            $(".signup-content").hide();
+            $(this).removeClass('hovered');
+        } ,500));
+    });
+
+
+$('.signup-content').hover(
+    function(){
+        clearTimeout($(".signup-btn").data('hoverTimeoutId'));
+    },
+    function(){
+        $(".signup-content").hide();
+        $(".signup-btn").removeClass('hovered');
+    });
+
 });
+
+
