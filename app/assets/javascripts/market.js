81d7786090d2f198c49977d97fa788d86241d29c
James Davis <jamesmdavis5@gmail.com>
2013-11-11 19:40:44 -0500

    merged seed file

--- app/assets/javascripts/market.js
+++ app/assets/javascripts/market.js
@@ -2,64 +2,68 @@ $(document).ready(function() {
 
     // $('#market').accordion();
 
-    $(".login").on("click", function(e) {
-        e.preventDefault();
-        $("fieldset#login_menu").toggle();
-        $(".login").toggleClass("menu-open");
-    });
-    $("fieldset#login_menu").mouseup(function() {
-        return false
-    });
-    $(document).mouseup(function(e) {
-        if($(e.target).parent("a.login").length==0) {
-            $(".login").removeClass("menu-open");
-            $("fieldset#login_menu").hide();
-        }
-    });
+// When you click on "LOG IN" link in header
+// it will toggle the form to log in
+$(".login").on("click", function(e) {
+  e.preventDefault();
+  $("fieldset#login_menu").toggle();
+  $(".login").toggleClass("menu-open");
+});
+$("fieldset#login_menu").mouseup(function() {
+  return false
+});
+$(document).mouseup(function(e) {
+  if($(e.target).parent("a.login").length==0) {
+    $(".login").removeClass("menu-open");
+    $("fieldset#login_menu").hide();
+  }
+});
 
-
-    $(".signup").on("click", function(e) {
-        e.preventDefault();
-        $("fieldset#signup_menu").toggle();
-        $(".signup").toggleClass("menu-open");
-    });
-    $("fieldset#signup_menu").mouseup(function() {
-        return false
-    });
-    $(document).mouseup(function(e) {
-        if($(e.target).parent("a.signup").length==0) {
-            $(".signup").removeClass("menu-open");
-            $("fieldset#signup_menu").hide();
-        }
-    });
+// When you click on "SIGN UP" link in header
+// It toggles the sign up form
+$(".signup").on("click", function(e) {
+  e.preventDefault();
+  $("fieldset#signup_menu").toggle();
+  $(".signup").toggleClass("menu-open");
+});
+$("fieldset#signup_menu").mouseup(function() {
+  return false
+});
+$(document).mouseup(function(e) {
+  if($(e.target).parent("a.signup").length==0) {
+    $(".signup").removeClass("menu-open");
+    $("fieldset#signup_menu").hide();
+  }
+});
 
 });
 
 
 
 $(document).ready(function(){
-  $("#signup").on("submit", createUser);
+  // When you click the sign up button on the form
+  // It should create a new user in the database
+  $("#signup_form").on("submit", createUser);
 });
 
 
 var createUser = function(e) {
   e.preventDefault();
-  var email = $("#email").val();
+  var $email = $("#signup_email").val();
 
 
   var newUserParams = {
     user: {
-      email: email,
+      email: $email,
     }
   }
-debugger
   $.ajax({
     type: "POST",
     url: "/users.json",
     data: newUserParams
   }).done(function(data) {
-  console.log(data);
-  $("form")[0].reset();
+    console.log(data);
+    // $("form")[0].reset();
   // $("#notice").append($("<p>").text("Created " + data.name).delay(2000).fadeOut(400));
 });
 }
