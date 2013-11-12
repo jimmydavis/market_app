b907b1f31a4586871fc78fcf5b97b45fe4e8fed6
James Davis <jamesmdavis5@gmail.com>
2013-11-11 20:25:48 -0500

    renamed js file, worked on markets model, views, and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require "csv"
 Market.delete_all
 
 # TODO -- may need to change path to get this to work in Heroku
-file = File.open("/Users/thomasmetzger/ga_wdi/heroku/market_app/db/20131111_nycmarket.csv","r")
+file = File.open("/Users/jamesmdavis5/ga_wdi/heroku/project_two/market_app/db/20131111_nycmarket.csv","r")
 
 CSV.parse(file, :headers => true) do |row|
   Market.create! ({
