b4b17843cfcc1c224d77ac67c4402c5814d04911
Tom Metzger <metzgert@gmail.com>
2013-11-11 22:19:36 -0500

    corrected name error in dates table migration file

--- db/migrate/20131112025611_dates.rb
+++ db/migrate/20131112025611_dates.rb
@@ -1,6 +1,6 @@
 class Dates < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :dates do |t|
       t.integer :market_id
       t.date :date_open
       t.integer :week_number
